d7dce162d6f39bc7a5fa5baf9bf49dcbe38325aa
GerganaRibarova <geri.ribarova@gmail.com>
2017-01-29 14:14:18 +0200

    Added the project

---
+++ AutomatedTellerMachine/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomatedTellerMachine.Controllers
+{
+    public class HomeController : Controller
+    {
+        //GET /home/index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET /home/about
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Having trouble? Send us a message.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            ViewBag.Message = "Thanks, we got your message. ";
+            return View();
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPNETMVC5ATM1";
+            if(letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+            return RedirectToAction("Index");
+        }
+    }
+
+}
